f3114885e47c655431307346856a2ae7d0898ec0
PabloHD2023 <demartinipablo@gmail.com>
2024-02-20 22:41:40 -0300

    Creando estructura y clases del model

---
+++ src/main/java/com/ecommerce/SpringECommerceApplication.java
@@ -0,0 +1,15 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class SpringECommerceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringECommerceApplication.class, args);
+    }
+
+}
