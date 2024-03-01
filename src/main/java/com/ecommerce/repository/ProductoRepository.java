ae8a04b5492f33646293562a4ee9bb130b14283f
PabloHD2023 <demartinipablo@gmail.com>
2024-03-01 15:03:08 -0300

    agregado de la pagina shop

---
+++ src/main/java/com/ecommerce/repository/ProductoRepository.java
@@ -0,0 +1,12 @@
+package com.ecommerce.repository;
+
+import com.ecommerce.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+
+
+
+}
